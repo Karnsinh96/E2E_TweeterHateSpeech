74264f4ae747daf72c094bc531066214e8d17952
Karnsinh Chikane <karnsinh96@gmail.com>
2024-04-02 17:23:52 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="TweetHateSpeech",
+    version="0.0.1",
+    author="Karnsinh96",
+    author_email="karnsinh96@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
